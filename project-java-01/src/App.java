02b737dfecc82772352302dfe5bf052489156b0c
Joao Borgato <joao_borgato@hotmail.com>
2023-06-08 18:38:09 -0300

    tst

--- project-java-01/src/App.java
+++ project-java-01/src/App.java
@@ -1,5 +1,17 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Vai Corinthians!");
+    public static void main (String[] args) throws Exception {
+        
+        String primeiroNome = "Joao";
+        String segundoNome = "Marcos";
+
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+
+        return primeiroNome.concat(" ").concat(segundoNome);
+        
     }
 }
